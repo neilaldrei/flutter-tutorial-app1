a21c60ea8535a5f7f3568a2716e2dc73684b90a8
neilaldrei <neilaldreiiv@gmail.com>
2020-03-11 09:36:18 +0800

    Lesson #5: Scaffold and App Bar widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Hey epic gamers!')
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("Good morning gamers!"),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text("Hello boi"),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('ClickClack')
+    ),
+  ),
 ));
