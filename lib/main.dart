183ac33f2ba497061bbc7289cf0c102d51f27bda
neilaldrei <neilaldreiiv@gmail.com>
2020-03-11 10:17:09 +0800

    Lesson #8: Images & Assets

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          "Hello boi",
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower'
-          ),
-        ),
+        child: Image.asset('assets/full-moon.jpg')
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
